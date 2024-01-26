5b63e9db435cd43942e3f38e716986234f2da76f
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-26 17:19:35 -0500

    adding login function in UsuarioViewModel

---
+++ app/src/main/java/com/dsa/lupiapp/viewmodel/UsuarioViewModel.java
@@ -0,0 +1,24 @@
+package com.dsa.lupiapp.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.dsa.lupiapp.entity.GenericResponse;
+import com.dsa.lupiapp.entity.service.Usuario;
+import com.dsa.lupiapp.repository.UsuarioRepository;
+
+public class UsuarioViewModel extends AndroidViewModel {
+
+    private final UsuarioRepository repository;
+    public UsuarioViewModel(@NonNull Application application) {
+        super(application);
+        this.repository = UsuarioRepository.getInstance();
+    }
+
+    public LiveData<GenericResponse<Usuario>> login(String email, String pass) {
+        return this.repository.login(email, pass);
+    }
+}
