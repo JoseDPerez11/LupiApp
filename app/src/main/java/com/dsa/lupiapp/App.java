39aff2f9997a204f16092f3e36560e666336d2ba
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-25 23:57:49 -0500

    App class creation

--- app/src/main/java/com/dsa/lupiapp/App.java
+++ app/src/main/java/com/dsa/lupiapp/App.java
@@ -1,4 +1,42 @@
 package com.dsa.lupiapp;
 
-public class App {
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.stetho.DumperPluginsProvider;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.dumpapp.DumperPlugin;
+
+
+// configura Stetho en la aplicación Android para permitir la depuración a través de Chrome Developer Tools.
+public class App extends Application {
+
+    private Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this;
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(context)
+                        .enableDumpapp((DumperPluginsProvider) new SampleDumperPluginsProvider(context))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(context))
+                        .build()
+        );
+    }
+
+    public static class SampleDumperPluginsProvider implements DumperPluginsProvider {
+
+        private Context context;
+
+        public SampleDumperPluginsProvider(Context context) { this.context = context; }
+        @Override
+        public Iterable<DumperPlugin> get() {
+
+            return Stetho.defaultDumperPluginsProvider(context).get();
+        }
+    }
+
 }
