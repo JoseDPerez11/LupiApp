6d41e5c146be23c89e6f0c74a9cee1834b03865d
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-27 12:51:57 -0500

    adding functionality to validate text inputs and we also added the error toast

--- app/src/main/java/com/dsa/lupiapp/activity/MainActivity.java
+++ app/src/main/java/com/dsa/lupiapp/activity/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,52 +50,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+
         // Se establece un listener para el evento de clic en el botón de inicio de sesión
         binding.btnIniciarSesion.setOnClickListener(viewModel -> {
-            // Se obtienen los valores ingresados por el usuario en los campos de correo y contraseña
-            String email = binding.edtMail.getText().toString();
-            String password = binding.edtPassword.getText().toString();
+            try {
+                if (validar()) {
+                    // Se obtienen los valores ingresados por el usuario en los campos de correo y contraseña
+                    String email = binding.edtMail.getText().toString();
+                    String password = binding.edtPassword.getText().toString();
 
-            // Se realiza el intento de inicio de sesión a través del ViewModel asociado al usuario
-            usuarioViewModel.login(email, password).observe(this, response -> {
+                    // Se realiza el intento de inicio de sesión a través del ViewModel asociado al usuario
+                    usuarioViewModel.login(email, password).observe(this, response -> {
 
-                // Se verifica la respuesta obtenida del intento de inicio de sesión
-                if (response.getRpta() == 1) {
-                    // Si la respuesta es exitosa (rpta = 1), se muestra un mensaje y se realiza el siguiente conjunto de acciones
-                    toastCorrecto(response.getMessage());
+                        // Se verifica la respuesta obtenida del intento de inicio de sesión
+                        if (response.getRpta() == 1) {
+                            // Si la respuesta es exitosa (rpta = 1), se muestra un mensaje y se realiza el siguiente conjunto de acciones
+                            toastCorrecto(response.getMessage());
 
-                    // Se obtiene el objeto Usuario desde la respuesta
-                    Usuario usuario = response.getBody();
+                            // Se obtiene el objeto Usuario desde la respuesta
+                            Usuario usuario = response.getBody();
 
-                    // Se guarda el objeto Usuario en SharedPreferences para persistencia entre sesiones
-                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                            // Se guarda el objeto Usuario en SharedPreferences para persistencia entre sesiones
+                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
 
-                    // Se utiliza Gson para convertir el objeto Usuario a formato JSON y guardarlo en SharedPreferences
-                    final Gson gson = new GsonBuilder()
-                            .registerTypeAdapter(Date.class, new DateSerializer())
-                            .registerTypeAdapter(Time.class, new TimeSerializer())
-                            .create();
-                    editor.putString("UsuarioJson", gson.toJson(usuario, new TypeToken<Usuario>(){
+                            // Se utiliza Gson para convertir el objeto Usuario a formato JSON y guardarlo en SharedPreferences
+                            final Gson gson = new GsonBuilder()
+                                    .registerTypeAdapter(Date.class, new DateSerializer())
+                                    .registerTypeAdapter(Time.class, new TimeSerializer())
+                                    .create();
+                            editor.putString("UsuarioJson", gson.toJson(usuario, new TypeToken<Usuario>(){
 
-                    }.getType()));
+                            }.getType()));
 
-                    editor.apply();
+                            editor.apply();
 
-                    // Se limpian los campos de correo y contraseña
-                    binding.edtMail.setText("");
-                    binding.edtPassword.setText("");
+                            // Se limpian los campos de correo y contraseña
+                            binding.edtMail.setText("");
+                            binding.edtPassword.setText("");
 
-                    // Se inicia una nueva actividad (InicioActivity) después de un inicio de sesión exitoso
-                    startActivity(new Intent(this, InicioActivity.class));
+                            // Se inicia una nueva actividad (InicioActivity) después de un inicio de sesión exitoso
+                            startActivity(new Intent(this, InicioActivity.class));
+
+                        } else {
+                            // Si la respuesta no es exitosa, se muestra un mensaje de error
+                            toastIncorrecto("Credenciales inválidas");
+                        }
+                    });
 
                 } else {
-                    // Si la respuesta no es exitosa, se muestra un mensaje de error
-                    Toast.makeText(this, "Ocurrió un error " + response.getMessage(), Toast.LENGTH_SHORT).show();
+                    toastIncorrecto("Por favor, complete todos los campos");
                 }
-            });
+            } catch (Exception e) {
+                toastIncorrecto("Se ha producido un error al intentar loguearte " + e.getMessage());
+            }
+
         });
+
+        binding.edtMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                binding.txtInputUsuario.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        binding.edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                binding.txtInputPassword.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+            
     }
+
 
     public void toastCorrecto(String message) {
 
@@ -108,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void toastIncorrect(String message) {
+    public void toastIncorrecto(String message) {
 
         CustomToastErrorBinding bindingToastErrpr = CustomToastErrorBinding.inflate(getLayoutInflater());
         bindingToastErrpr.txtMensajeToastError.setText(message);
@@ -118,6 +159,24 @@ public class MainActivity extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(bindingToastErrpr.getRoot());
         toast.show();
+    }
+
+    private boolean validar() {
+        boolean valorRetorno = true;
+        String usuario = binding.edtMail.getText().toString();
+        String password = binding.edtPassword.getText().toString();
+
+        if (usuario.isEmpty()) {
+            binding.txtInputUsuario.setError("Ingrese su usuario y/o correo electrónico");
+            valorRetorno = false;
+        } else { binding.txtInputUsuario.setErrorEnabled(false); }
+
+        if (password.isEmpty()) {
+            binding.txtInputPassword.setError("Ingrese su contraseña");
+            valorRetorno = false;
+        } else { binding.txtInputPassword.setErrorEnabled(false); }
+
+        return valorRetorno;
     }
 
 }
