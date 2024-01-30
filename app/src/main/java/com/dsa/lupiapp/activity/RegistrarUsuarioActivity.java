75adb205e7bda160a335301eb62aee8359bf8b23
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-30 17:38:31 -0500

    using SweetAler for notification messages

--- app/src/main/java/com/dsa/lupiapp/activity/RegistrarUsuarioActivity.java
+++ app/src/main/java/com/dsa/lupiapp/activity/RegistrarUsuarioActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import com.dsa.lupiapp.R;
 import com.dsa.lupiapp.databinding.ActivityRegistrarUsuarioBinding;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class RegistrarUsuarioActivity extends AppCompatActivity {
 
     private ActivityRegistrarUsuarioBinding binding;
@@ -18,4 +20,21 @@ public class RegistrarUsuarioActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
     }
+
+
+    public void succesMessage(String message) {
+        new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE).setTitleText("Buen trabajo")
+                .setContentText(message).show();
+    }
+
+    public void errorMessage(String message) {
+        new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE).setContentText("Ops...")
+                .setContentText(message).show();
+    }
+
+    public void warningMessage(String message) {
+        new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE).setContentText("Notificación")
+                .setContentText(message).show();
+    }
+
 }
