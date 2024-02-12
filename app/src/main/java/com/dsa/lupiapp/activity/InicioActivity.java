2b91ff5c9b8c3570e4ad267e2ced8608dd2ab61f
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-02-12 15:14:50 -0500

    comments in InicioActivity

--- app/src/main/java/com/dsa/lupiapp/activity/InicioActivity.java
+++ app/src/main/java/com/dsa/lupiapp/activity/InicioActivity.java
@@ -76,12 +76,19 @@ public class InicioActivity extends AppCompatActivity {
         return true;
     }
 
+    // Método que maneja los eventos cuando se selecciona un elemento del menú
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Verifica si el ID del elemento del menú seleccionado es igual al ID del elemento "cerrarSesion"
         if (item.getItemId() == R.id.cerrarSesion) {
+            // Si es así, llama al método logout() para cerrar la sesión del usuario
             this.logout();
+            // Indica que el evento de selección del menú ha sido manejado correctamente
             return true;
         }
+
+        // Si el elemento del menú seleccionado no es "cerrarSesion",
+        // delega el manejo del evento al método onOptionsItemSelected de la clase base
         return super.onOptionsItemSelected(item);
     }
 
@@ -91,40 +98,67 @@ public class InicioActivity extends AppCompatActivity {
         loadData();
     }
 
+    // Método para cargar datos del usuario desde SharedPreferences y mostrarlos en la vista
     private void loadData() {
+
+        // Obtener las preferencias compartidas
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Crear un objeto Gson para serializar y deserializar objetos JSON
         final Gson gson = new GsonBuilder()
+                // Registrar adaptadores personalizados para serializar/deserializar objetos Date y Time
                 .registerTypeAdapter(Date.class, new DateSerializer())
                 .registerTypeAdapter(Time.class, new TimeSerializer())
                 .create();
-
+        // Obtener el JSON del usuario almacenado en SharedPreferences
         String usuarioJson = sharedPreferences.getString("UsuarioJson", null);
+
+        // Verificar si el JSON del usuario no es nulo
         if (usuarioJson != null) {
+            // Deserializar el JSON del usuario en un objeto Usuario utilizando Gson
             final Usuario usuario = gson.fromJson(usuarioJson, Usuario.class);
+
+            // Obtener la vista del encabezado del NavigationView
             final View viewHeader = binding.navView.getHeaderView(0);
+
+            // Obtener referencias a los TextView y CircleImageView dentro del encabezado
             final TextView tvNombre = viewHeader.findViewById(R.id.tvNombre);
             final TextView tvCorreo =  viewHeader.findViewById(R.id.tvCorreo);
             final CircleImageView imgFoto = viewHeader.findViewById(R.id.imageView);
 
+            // Establecer el nombre y correo del usuario en los TextView correspondientes
             tvNombre.setText(usuario.getCliente().getNombreCompleto());
             tvCorreo.setText(usuario.getEmail());
 
+            // Construir la URL de la imagen del usuario
             String url = ConfigApi.baseUrlE + "/api/documento-almacenado/download/" + usuario.getCliente().getFoto().getFileName();
+
+            // Crear un objeto Picasso para cargar la imagen desde la URL
             final Picasso picasso = new Picasso.Builder(this)
                     .downloader(new OkHttp3Downloader(ConfigApi.getClient()))
                     .build();
+            // Cargar la imagen desde la URL utilizando Picasso y establecer una imagen de error en caso de fallo
             picasso.load(url)
                     .error(R.drawable.image_not_found)
                     .into(imgFoto);
         }
     }
 
+    // Método para cerrar sesión del usuario
     private void logout() {
+        // Obtener las preferencias compartidas
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Crear un editor de preferencias para realizar cambios
         SharedPreferences.Editor editor = preferences.edit();
+
+        // Eliminar la clave "UsuarioJson" de las preferencias compartidas, lo que equivale a cerrar sesión
         editor.remove("UsuarioJson");
+        // Aplicar los cambios al editor de preferencias
         editor.apply();
+        // Finalizar la actividad actual
         this.finish();
+        // Aplicar una animación de transición al cerrar la actividad
         this.overridePendingTransition(R.anim.left_in, R.anim.left_out);
     }
 
